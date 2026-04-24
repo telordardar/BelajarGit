e502fd51f90907764427ad40c7d027a488991775
telordardar <haidarfaizputrasulistyo@gmail.com>
2026-04-24 09:48:34 +0700

    fix: implement pause and resume state

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro; // 1. Don't forget this!
 
 public class UIManager : MonoBehaviour
 {
+    // 2. This must be INSIDE the class brackets
+    [SerializeField] private TextMeshProUGUI buttonText;
+
     public void StartGame()
     {
+        Time.timeScale = 1f; // Always reset time when starting/restarting
         SceneManager.LoadScene("Game");
     }
 
@@ -15,6 +20,22 @@ public class UIManager : MonoBehaviour
 
     public void Restart()
     {
+        Time.timeScale = 1f; 
         SceneManager.LoadScene("Game");
+    }
+    
+    public void TogglePause()
+    {
+            
+        if (GameManager.Instance.currentState == GameState.Playing)
+        {
+            GameManager.Instance.PauseGame();
+            buttonText.text = "Resume";
+        }
+        else if (GameManager.Instance.currentState == GameState.Paused)
+        {
+            GameManager.Instance.ResumeGame();
+            buttonText.text = "Pause";
+        }
     }
 }
