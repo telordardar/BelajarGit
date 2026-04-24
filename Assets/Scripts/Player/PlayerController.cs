b27bc224455d9cd654110b5b4a5c941254b1f01a
telordardar <haidarfaizputrasulistyo@gmail.com>
2026-04-24 09:51:26 +0700

    fix: connect player to scriptable object

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,43 +3,54 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float currentHP = 100;
-    public float speed = 5f;
+    public PlayerData playerData;
+    private float currentHP;
+    
+    private float speed;
     private PlayerInput playerInput;
     private Vector2 moveInput;
 
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
+        currentHP = playerData.maxHP;
+        speed = playerData.moveSpeed;
     }
-    
-    
+
     void Update()
     {
+        if (GameManager.Instance.currentState != GameState.Playing) return;
         if (playerInput == null) return;
-        
-        moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
-        float h = moveInput.x;
-        float v = moveInput.y;
 
-        transform.Translate(new Vector3(h, v, 0) * speed * Time.deltaTime);
+        moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
+        transform.Translate(new Vector3(moveInput.x, moveInput.y, 0) * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            TakeDamage(playerData.maxHP); 
+            Debug.Log("Player jatuh ke lantai!");
+        }
     }
 
     void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Wall"))
         {
-            TakeDamage(0.1f);
+            TakeDamage(10.0f * Time.deltaTime);
         }
     }
 
     void TakeDamage(float dmg)
     {
-        currentHP -= dmg;
-        Debug.Log("Player HP: " + currentHP);
+        if (GameManager.Instance.currentState == GameState.GameOver) return;
 
+        currentHP -= dmg;
         if (currentHP <= 0)
         {
+            currentHP = 0;
             GameManager.Instance.GameOver();
         }
     }
--- Assets/Scripts/ScriptableObject/PlayerData.cs
+++ Assets/Scripts/ScriptableObject/PlayerData.cs
@@ -1,8 +1,8 @@
-using UnityEngine;
+   using UnityEngine;
 
 [CreateAssetMenu(fileName = "PlayerData", menuName = "Game/Player Data")]
 public class PlayerData : ScriptableObject
 {
-    public float maxHP;
+    public float maxHP; 
     public float moveSpeed;
 }
